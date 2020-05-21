854448fadb49bd7dea2d4ef4b35790310bc82835
Eric warren <ericwarren99@hotmail.com>
2020-05-21 19:43:25 -0400

    not sure what this is its been sitting on my computer for a while

--- src/mypkg/ip_address.py
+++ src/mypkg/ip_address.py
@@ -4,11 +4,10 @@ import re
 def is_mask(mask):
     re.search("^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$", mask)
 
-def is_ip(ip_in, mask=""):
+def get_ip(ip_in, mask=""):
     if mask == "":
         try:
-            return True
-            #return ipaddress.ip_address(ip_in)
+            return ipaddress.ip_address(ip_in)
         except ValueError: 
             try:
                 return ip_interface(ip_in)
--- tests/test_ip_address.py
+++ tests/test_ip_address.py
@@ -1,6 +1,7 @@
 import pytest
+import ipaddress
 
 from mypkg.ip_address import *
 
-def test_is_ip():
-	assert(is_ip("192.168.0.1") == True)
+def test_is_ip_normal():
+	assert(get_ip("192.168.0.1") == ipaddress.ip_address("192.168.0.1"))
