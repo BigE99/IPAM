b7afd7e9c298e528e83fcd8498ecdc0f7dd0b125
Eric Warren <ericwarren99@hotmail.com>
2020-05-27 10:36:08 -0400

    we will see if the import fixes the test issues

--- tests/test_ip_address.py
+++ tests/test_ip_address.py
@@ -1,8 +1,8 @@
 import pytest
 import ipaddress
 import mypkg.ip_address
-from pytest import assume
 
+'''
 @pytest.mark.parametrize(('ip', 'mask'), [("10.0.0.1", ""), ("10.0.0.1", "24"), ("10.0.0.1", "/24"), ("10.0.0.1", "255.255.255.0"), ("10.0.0.0", "")])
 def test_get_ip_ipv4(ip, mask):
 	pytest.assume(ipaddress.ip_address(ip) == get_ip(ip))
@@ -12,4 +12,10 @@ def test_get_ip_ipv4(ip, mask):
 	pytest.assume(not get_ip(ip))
 	
 def test_get_ip_ipv6(ip, mask):
-	return 0
+	return 0'''
+@pytest.mark.parametrize(('x', 'y'), [(1, 1), (1, 0), (0, 1)])
+def test_simple_assume(x, y):
+    with pytest.assume:
+        assert x == y
+        assert True
+        assert False
