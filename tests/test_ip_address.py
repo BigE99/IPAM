155af20d541819a4c8f0ec079f95bade1ee2e4c3
Eric Warren <ericwarren99@hotmail.com>
2020-05-27 10:43:50 -0400

    testing assumes in test cases

--- tests/test_ip_address.py
+++ tests/test_ip_address.py
@@ -15,7 +15,6 @@ def test_get_ip_ipv6(ip, mask):
 	return 0'''
 @pytest.mark.parametrize(('x', 'y'), [(1, 1), (1, 0), (0, 1)])
 def test_simple_assume(x, y):
-    with pytest.assume:
-        assert x == y
-        assert True
-        assert False
+    pytest.assume(x == y)
+    pytest.assume(True)
+    pytest.assume(False)
